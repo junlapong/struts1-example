ea000840c39e4b983c72e4c5552bcdbf02f7a413
Junlapong L <junlapong@gmail.com>
2016-11-02 03:31:52 +0700

    add urlrewrite

--- src/main/java/com/mkyong/common/action/HelloWorldAction.java
+++ src/main/java/com/mkyong/common/action/HelloWorldAction.java
@@ -11,14 +11,14 @@ import org.apache.struts.action.ActionMapping;
 import com.mkyong.common.form.HelloWorldForm;
 
 public class HelloWorldAction extends Action{
-	
+
 	public ActionForward execute(ActionMapping mapping,ActionForm form,
 			HttpServletRequest request,HttpServletResponse response) throws Exception {
-		
+
 		HelloWorldForm helloWorldForm = (HelloWorldForm) form;
-		helloWorldForm.setMessage("Hello World! Struts");
-		
+		helloWorldForm.setMessage("Hello World! Struts @" + new java.util.Date());
+
 		return mapping.findForward("success");
 	}
-	
+
 }
